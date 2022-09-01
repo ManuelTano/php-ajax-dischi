d648ba0f740878bd43042779dd83fde1eb2e0774
ManuelTano <manueltano98@gmail.com>
2022-09-01 16:34:21 +0200

    PHP Version Completed

--- php-version/index.php
+++ php-version/index.php
@@ -1,5 +1,83 @@
+<?php
+
+$discs = [
+    [
+        'title' => 'New Jersey',
+        'author' => 'Bon Jovi',
+        'year' => 1988,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
+        'genre' => 'Rock'
+    ],
+    [
+        'title' => 'Live at Wembley 86',
+        'author' => 'Queen',
+        'year' => 1992,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
+        'genre' => 'Pop'
+    ],
+    [
+        'title' => 'Ten\'s Summoner\'s Tales',
+        'author' => 'Sting',
+        'year' => 1993,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
+        'genre' => 'Pop'
+    ],
+    [
+        'title' => 'Steve Gadd band',
+        'author' => 'Steve Gadd Band',
+        'year' => 2018,
+        'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
+        'genre' => 'Jazz'
+    ],
+    [
+        'title' => 'Brave new World',
+        'author' => 'Iron Maiden',
+        'year' => 2000,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
+        'genre' => 'Metal'
+    ],
+    [
+        'title' => 'One more car, one more rider',
+        'author' => 'Eric Clapton',
+        'year' => 2002,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
+        'genre' => 'Rock'
+    ],
+    [
+        'title' => 'Ten\'s Summoner\'s Tales',
+        'author' => 'Sting',
+        'year' => 1993,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
+        'genre' => 'Pop'
+    ],
+    [
+        'title' => 'Steve Gadd band',
+        'author' => 'Steve Gadd Band',
+        'year' => 2018,
+        'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
+        'genre' => 'Jazz'
+    ],
+    [
+        'title' => 'Brave new World',
+        'author' => 'Iron Maiden',
+        'year' => 2000,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
+        'genre' => 'Metal'
+    ],
+    [
+        'title' => 'One more car, one more rider',
+        'author' => 'Eric Clapton',
+        'year' => 2002,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
+        'genre' => 'Rock'
+    ]
+];
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,23 +92,34 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
+
 <body>
-<header>
-    <div class="row p-3 h-100 justify-content-between">
-      <div class="col-6 d-flex h-100 align-items-center">
-        <div class="image">
-          <img src="../img/spotify.png" alt="Spotify Logo" />
+    <header>
+        <div class="row p-3 h-100 justify-content-between">
+            <div class="col-6 d-flex h-100 align-items-center">
+                <div class="image">
+                    <img src="../img/spotify.png" alt="Spotify Logo" />
+                </div>
+            </div>
+            <div class="col-2 d-flex text-end h-100 align-items-center">
+            </div>
         </div>
-      </div>
-      <div class="col-2 d-flex text-end h-100 align-items-center">
-      </div>
-    </div>
-  </header>
-  <main>
-    <div class="container">
-      <div class="row py-5">
-      </div>
-    </div>
-  </main>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row py-5">
+                <?php
+                foreach ($discs as $disco) : ?>
+                    <div class="disc mx-3 my-4 text-center col-2 p-3">
+                        <img class="img-fluid" src="<?php echo $disco['poster'] ?>">
+                        <h5 class="mt-4 text-white"> <?php echo $disco['title'] ?></h5>
+                        <p class="grey"> <?php echo $disco['author'] ?>
+                        <p class="grey"> <?php echo $disco['year'] ?>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </main>
 </body>
+
 </html>
